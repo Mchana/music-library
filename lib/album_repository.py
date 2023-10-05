ed5801fa0f2260263786ddd642b4c9e6d9a62ee5
Tej <muntejchana@hotmail.com>
2023-10-05 11:56:13 +0100

    put id attribute in

--- lib/album_repository.py
+++ lib/album_repository.py
@@ -1,2 +1,15 @@
+from lib.album import Album
+
 class AlbumRepository:
-    pass
+    
+    def __init__(self,connection):
+        self.connection = connection
+
+    def all(self,connection):
+        rows = self.connection.excecute("SELECT * FROM albums")
+        albums = []
+        for row in rows:
+            album = Album(row ["id"],row["title"],row["release_year"],row["artist_id"])
+            albums.append(album)
+        
+        return albums
--- tests/test_album.py
+++ tests/test_album.py
@@ -1,7 +1,8 @@
 from lib.album import Album
 
 def test_album_attributes():
-    album = Album('Doolittle',1984,1)
+    album = Album(1,'Doolittle',1984,1)
+    assert album.id == 1
     assert album.title == 'Doolittle'
     assert album.release_year == 1984
     assert album.artist_id == 1
